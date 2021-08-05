9507af78acd31d4cb5f75e261f4454c2b5f9f44e
Pax <13646646+paxcodes@users.noreply.github.com>
2021-08-04 17:21:28 -0700

    feat(activity1.01): create welcome screen

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    name = request.GET.get("name") or "world"
-    return render(request, "base.html", {"name": name})
+    return render(request, "base.html")
