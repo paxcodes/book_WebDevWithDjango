022f5a258cb9a8349be1ed2c76b8421b3f753a17
Pax <13646646+paxcodes@users.noreply.github.com>
2021-08-06 06:53:47 -0700

    feat(ex3.02): use templates

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Book
 
 def welcome_view(request):
-    message = f"<html><h1>Welcome to Bookr!</h1> Something more her {Book.objects.count()}</html>"
-    return HttpResponse(message)
+    return render(request, "base.html")
