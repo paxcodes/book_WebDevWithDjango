d0a05852ec1b312aa5d01cbd41ca24751e959ed1
Pax <13646646+paxcodes@users.noreply.github.com>
2021-08-07 19:08:02 -0700

    feat: use function for admin interface of Contributor

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -3,12 +3,22 @@ from django.contrib import admin
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
 
+def initialed_name(contributor):
+    """Name of contributor with only first names' initials. E.g. Pax, RW"""
+    fn_initials = ''.join(name[0] for name in contributor.first_names.split(' '))
+    return f"{contributor.last_names}, {fn_initials}"
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (initialed_name,)
+
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'isbn')
 
 
 admin.site.register(Publisher)
-admin.site.register(Contributor)
+admin.site.register(Contributor, ContributorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review)
