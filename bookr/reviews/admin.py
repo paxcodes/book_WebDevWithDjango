36d5bc17c6673069392f638f2b94b195e175c660
Pax <13646646+paxcodes@users.noreply.github.com>
2021-08-07 18:59:14 -0700

    feat(admin): add columns title, isbn in "Book List" page

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn')
+
+
 admin.site.register(Publisher)
 admin.site.register(Contributor)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review)
