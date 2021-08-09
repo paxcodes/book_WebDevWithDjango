a48de2f81d91bded01dee89775afcc0e2b997c24
Pax <13646646+paxcodes@users.noreply.github.com>
2021-08-09 05:47:28 -0700

    feat: explicitly specify fields in Review add/edit pages
    ...using fieldsets. date_edited field is excluded/removed from the add/edit pages.
    
    We can also use 'fields':
    fields = ('creator', 'book', 'content', 'rating')

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -18,9 +18,20 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'isbn13', 'publication_date', 'publisher')
     list_filter = ['publisher', 'publication_date']
 
+    # You can also do "dunder" matches like `__exact` `__startswith`
+    # E.g. isbn__exact will require the complete ISBN to be matched.
+    search_fields = ('title', 'isbn', 'publisher__name')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('creator', 'book')}),
+        ('Review content', {'fields': ('content', 'rating')}),
+    )
+
 
 admin.site.register(Publisher)
 admin.site.register(Contributor, ContributorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
