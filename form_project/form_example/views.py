6c0a4bd86e96b8463dd8c820902c2ae4ae49faf9
Pax <13646646+paxcodes@users.noreply.github.com>
2021-08-15 14:37:06 -0700

    feat: validate decimal input

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -8,7 +8,7 @@ def form_example(request):
     for name in request.POST:
         print(f"{name}: {request.POST.getlist(name)}")
 
-    form = ExampleForm()
+    form = ExampleForm(request.POST) if request.method == "POST" else ExampleForm()
     return render(
         request, "form-example.html", {"method": request.method, "form": form}
     )
