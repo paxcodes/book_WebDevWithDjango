8906d66d476b9bdb7af78bcc5a832e8f8aa3c74d
Pax <13646646+paxcodes@users.noreply.github.com>
2021-08-15 18:32:23 -0700

    fix: put back views and urls that were removed in ex3.1
    It was "magically" brought back in ex3.4 as seen in Packt's repo (e.g. https://github.com/PacktPublishing/Web-Development-with-Django/blob/master/Chapter03/Exercise3.04/bookr/reviews/views.py).
    
    But it was NOT mentioned at all in the book to bring it back.

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_details, name='book_details'),
+    path('books/search/', views.book_search, name="book_search"),
 ]
