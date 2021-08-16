53659c559ae14a3210a5315df055f5b9dc376c1d
Pax <13646646+paxcodes@users.noreply.github.com>
2021-08-15 17:59:49 -0700

    feat: refactor ex6.4 and redirect to success-page (see p325)

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -1,19 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
 from .forms import ExampleForm
 
 
 def form_example(request):
-    if request.method == "POST":
-        form = ExampleForm(request.POST)
-    else:
-        form = ExampleForm()
+    form = ExampleForm(request.POST) if request.method == "POST" else ExampleForm()
 
-    if request.method == "POST":
-        form = ExampleForm(request.POST)
-        if form.is_valid():
-            for name, value in form.cleaned_data.items():
-                print("{}: ({}) {}".format(name, type(value), value))
+    if form.is_valid():
+        # cleaned_data is only populated if the form is valid
+        for name, value in form.cleaned_data.items():
+            print(f"{name}: ({type(value)}) {value}")
+        # Redirect to a success page according to page 325
+        return redirect("/success-page/")
 
     return render(
         request, "form-example.html", {"method": request.method, "form": form}
     )
+
+
+# View function for the success page (according to page 325)
+def form_success(request):
+    return render(request, "form-success.html")
