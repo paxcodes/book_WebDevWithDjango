71b7483ba1fe1b00c9de1e35e587fce1621a09dd
Pax <13646646+paxcodes@users.noreply.github.com>
2021-09-04 11:49:48 -0700

    implement view that uses MEDIA_URL variable

--- media_project/media_project/urls.py
+++ media_project/media_project/urls.py
@@ -18,8 +18,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+import media_example.views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('media-example/', media_example.views.media_example),
 ]
 
 if settings.DEBUG:
