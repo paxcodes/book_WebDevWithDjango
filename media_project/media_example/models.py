c800beb69ccf0bcdaa213f9ef415b33e00141bf5
Pax <13646646+paxcodes@users.noreply.github.com>
2021-09-04 14:16:54 -0700

    create model with file+image fields and create migration for it

--- media_project/media_example/models.py
+++ media_project/media_example/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class ExampleModel(models.Model):
+    image_field = models.ImageField(upload_to="images/")
+    file_field = models.FileField(upload_to="files/")
