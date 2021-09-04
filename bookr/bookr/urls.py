05fe090745ec819076243ba58d1eacb39e3d57ae
Pax <13646646+paxcodes@users.noreply.github.com>
2021-09-04 14:42:06 -0700

    step2: map MEDIA_URL if environment is for development
    By doing this, we are letting django handle requests to static files (in this case, the media files). This is OK during development.
    
    If it's a production environment, we would want to have our static media files be handled by the web server (nginx) for better performance.

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # `include()` is a shortcut that allows you to combine URL configurations.
@@ -27,3 +30,8 @@ urlpatterns = [
     path("", include('reviews.urls')),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
