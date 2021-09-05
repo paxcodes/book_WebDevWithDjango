db0d73916f8f16541a8f33a60f370b995162a748
Pax <13646646+paxcodes@users.noreply.github.com>
2021-09-04 19:22:17 -0700

    map books/<pk>/media url to book_media view

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_details, name='book_details'),
+    path('books/<int:pk>/media', views.book_media, name='book_media_edit'),
     path('books/<int:book_pk>/reviews/new/', views.review_edit, name='review_create'),
     path(
         'books/<int:book_pk>/reviews/<int:review_pk>/',
