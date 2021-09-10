189f21b3e320805c0d6442bd99b8c55b892b040f
Pax <13646646+paxcodes@users.noreply.github.com>
2021-09-09 18:29:35 -0700

    use template in a view

--- bookr/filter_demo/views.py
+++ bookr/filter_demo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    names = "john,doe,mark,swain"
+    return render(request, "index.html", {"names": names})
