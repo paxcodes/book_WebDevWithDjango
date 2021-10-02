402a27a0c0cac21d836fd9c71f8b83f09d0ca9de
Pax <13646646+paxcodes@users.noreply.github.com>
2021-10-02 16:09:24 -0700

    use class-based view

--- bookr/reviews/api_views.py
+++ bookr/reviews/api_views.py
@@ -1,12 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework import generics
 from rest_framework.response import Response
 
 from .models import Book
 from .serializers import BookSerializer
 
 
-@api_view()
-def all_books(request):
-    books = Book.objects.all()
-    book_serializer = BookSerializer(books, many=True)
-    return Response(book_serializer.data)
+class AllBooks(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
