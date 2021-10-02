3ccc6993915d35d598c601fecc0e8409ed4c0146
Pax <13646646+paxcodes@users.noreply.github.com>
2021-10-02 16:34:14 -0700

    create serializers

--- bookr/reviews/serializers.py
+++ bookr/reviews/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Book, Publisher
+from .models import Book, BookContributor, Contributor, Publisher
 
 
 # We must create the `PublisherSerializer` to serialize the `publisher` field
@@ -12,6 +12,31 @@ class PublisherSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
+    publisher = PublisherSerializer()
+
     class Meta:
         model = Book
         fields = ['title', 'publication_date', 'isbn', 'publisher']
+
+
+class ContributionSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+
+    class Meta:
+        model = BookContributor
+        fields = ['book', 'role']
+
+
+class ContributorSerializer(serializers.ModelSerializer):
+    bookcontributor_set = ContributionSerializer(many=True, read_only=True)
+    contribution_count = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Contributor
+        fields = [
+            'first_names',
+            'last_names',
+            'email',
+            'bookcontributor_set',
+            'contribution_count',
+        ]
