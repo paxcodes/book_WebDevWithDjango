05b4d6d655f6105461f34cb824d752b6da2a0fd2
Pax <13646646+paxcodes@users.noreply.github.com>
2021-10-02 13:54:23 -0700

    map view to url

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views, api_views
 
 urlpatterns = [
-    path('api/first_api_view/', api_views.first_api_view),
+    path('api/books/', api_views.all_books, name='all_books'),
     path('', views.index),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_details, name='book_details'),
