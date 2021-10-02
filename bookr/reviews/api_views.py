2178c16aeb4bdc1d5b860710d8ab364e78b1245c
Pax <13646646+paxcodes@users.noreply.github.com>
2021-10-02 13:54:18 -0700

    use serializer in views

--- bookr/reviews/api_views.py
+++ bookr/reviews/api_views.py
@@ -2,9 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Book
+from .serializers import BookSerializer
+
 
 @api_view()
-def first_api_view(request):
-    num_books = Book.objects.count()
-    return Response({"num_books": num_books})
-    
+def all_books(request):
+    books = Book.objects.all()
+    book_serializer = BookSerializer(books, many=True)
+    return Response(book_serializer.data)
