eab031d64a0a6f6ff46dd264990ab362037b2b35
Pax <13646646+paxcodes@users.noreply.github.com>
2021-10-02 13:43:01 -0700

    Exercise 12.1: Create first API view

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from . import views
+from . import views, api_views
 
 urlpatterns = [
+    path('api/first_api_view/', api_views.first_api_view),
     path('', views.index),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_details, name='book_details'),
