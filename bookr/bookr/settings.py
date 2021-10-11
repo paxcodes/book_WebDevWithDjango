625aac1be2c29bd3a3d01c902d5c073aa6b552e7
Pax <13646646+paxcodes@users.noreply.github.com>
2021-10-11 15:17:49 -0700

    read database URL from environment

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -78,12 +78,7 @@ class Dev(Configuration):
     # Database
     # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    DATABASES = values.DatabaseURLValue(f'sqlite:///{BASE_DIR}/db.sqlite3', environ_prefix='DJANGO')
 
     # Password validation
     # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
